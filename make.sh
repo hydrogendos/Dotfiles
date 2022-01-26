1e300be12bed0f41eab7a2f1fd955c0d68381505
tora <hydrogendos@protonmail.com>
2022-01-25 21:58:25 -0500

    changed up the make script to work correctly

--- make.sh
+++ make.sh
@@ -6,11 +6,14 @@
 
 ########## Variables
 
-# dotfiles directory
+# source dotfiles directory
+sourcedir=/media/tora/Storage/Prog/Dotfiles
+
+# dotfiles directory that is actually being used
 dir=/home/tora/Prog/Dotfiles
 
 # old dotfiles backup directory
-olddir=/home/tora/Prog/Dotfiles_Old
+olddir=/media/tora/Storage/Prog/Dotfiles_Old
 
 # list of files/folders to symlink in homedir
 files="bashrc bash_aliases tmux.conf"
@@ -18,14 +21,21 @@ files="bashrc bash_aliases tmux.conf"
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup of any existing dotfiles in $dir ..."
 mkdir -p $olddir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# copy this script to homedir
+echo -n "Copying script to $dir ..."
+cp -f $sourcedir/make.sh $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, and copy dotfiles from storage into the homedir, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
+    echo "Backing up any existing dotfiles from $dir to $olddir"
+    mv $dir/$file $olddir
+    echo "Copying dotfiles from $sourcedir to $dir"
+    cp -f $sourcedir/$file $dir
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -sf $dir/$file ~/.$file
 done
