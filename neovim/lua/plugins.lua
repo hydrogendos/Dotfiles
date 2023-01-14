1872d12d8ebaa4b3aaa8a5e5c4d3d3557c89ff89
tora <hydrogendos@protonmail.com>
2023-01-13 20:53:09 -0500

    added plugins for neovim

---
+++ neovim/lua/plugins.lua
@@ -0,0 +1,6 @@
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
+
+return require("packer").startup(function(use)
+  use ("wbthomason/packer.nvim")
+  use ("Olical/conjure")
+end)
